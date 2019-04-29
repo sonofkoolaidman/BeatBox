9d07c3180d4fff814a0ac21f85650373ba8f77ad
sonofkoolaidman <35243264+sonofkoolaidman@users.noreply.github.com>
2019-04-29 09:02:23 -0500

    added more fragment files.

--- app/src/main/java/com/ctech/max/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/ctech/max/beatbox/BeatBoxFragment.java
@@ -1,10 +1,31 @@
 package com.ctech.max.beatbox;
 
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ctech.max.beatbox.databinding.FragmentBeatBoxBinding;
 
 public class BeatBoxFragment extends Fragment {
 
     public static BeatBoxFragment newInstance() {
         return new BeatBoxFragment();
+    }
+
+    @Override public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                       Bundle savedInstanceState) {
+
+        // use the DataBindingUtil to inflate our layout
+        FragmentBeatBoxBinding binding = DataBindingUtil.inflate(
+                inflater, R.layout.fragment_beat_box, container, false);
+
+        // configure the recyclerView to use a GridLayoutManager with 3 columns
+        binding.recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+
+        return binding.getRoot();
     }
 }
