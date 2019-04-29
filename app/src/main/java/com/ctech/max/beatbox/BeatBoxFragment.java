d1460aae2c5aafce90fc001fdabd5fd208732e52
sonofkoolaidman <35243264+sonofkoolaidman@users.noreply.github.com>
2019-04-29 08:43:24 -0500

    Initial Commit.

---
+++ app/src/main/java/com/ctech/max/beatbox/BeatBoxFragment.java
@@ -0,0 +1,10 @@
+package com.ctech.max.beatbox;
+
+import android.support.v4.app.Fragment;
+
+public class BeatBoxFragment extends Fragment {
+
+    public static BeatBoxFragment newInstance() {
+        return new BeatBoxFragment();
+    }
+}
