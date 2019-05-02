51a5645880eca5abf71daccf69ab90dba58fae8f
sonofkoolaidman <35243264+sonofkoolaidman@users.noreply.github.com>
2019-05-02 08:22:23 -0500

    Sounds now play when pressing button.

--- app/src/main/java/com/ctech/max/beatbox/BeatBox.java
+++ app/src/main/java/com/ctech/max/beatbox/BeatBox.java
@@ -1,7 +1,10 @@
 package com.ctech.max.beatbox;
 
 import android.content.Context;
+import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.util.Log;
 
 import java.io.IOException;
@@ -13,13 +16,25 @@ public class BeatBox {
     public static final String TAG = "BeatBox";
 
     public static final String SOUNDS_FOLDER = "sample_sounds";
+    private static final int MAX_SOUNDS = 5;
 
     private AssetManager mAssets;
     private List<Sound> mSoundList = new ArrayList<>();
+    private SoundPool mSoundPool;
 
     public BeatBox(Context context) {
         mAssets = context.getAssets();
+        // This old constructor is deprecated but we need it for backwards compatibility
+        mSoundPool = new SoundPool(MAX_SOUNDS, AudioManager.STREAM_MUSIC, 0);
         loadSounds();
+    }
+
+    public void play(Sound sound) {
+        Integer soundId = sound.getSoundId();
+        if (soundId == null) {
+            return;
+        }
+        mSoundPool.play(soundId, 1.0f, 1.0f, 1, 0, 1.0f);
     }
 
     private void loadSounds() {
@@ -33,10 +48,22 @@ public class BeatBox {
         }
 
         for (String filename : soundNames) {
-            String assetPath = SOUNDS_FOLDER + "/" + filename;
-            Sound mySound = new Sound(assetPath);
-            mSoundList.add(mySound);
+            try {
+                String assetPath = SOUNDS_FOLDER + "/" + filename;
+                Sound mySound = new Sound(assetPath);
+                load(mySound);
+                mSoundList.add(mySound);
+            } catch (IOException ioe) {
+                Log.e(TAG, "Could not load sound from file: " + filename, ioe);
+            }
         }
+    }
+
+    //load an individual sound from a file and store the Id in a Sound object
+    private void load(Sound sound) throws IOException {
+        AssetFileDescriptor afd = mAssets.openFd(sound.getAssetPath());
+        int soundId = mSoundPool.load(afd, 1);
+        sound.setSoundId(soundId);
     }
 
     public List<Sound> getSoundList() {
